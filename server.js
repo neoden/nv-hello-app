ba719b682deffb9e5b3a181326f0ff127510d32d
neoden <lenar.imamutdinov@gmail.com>
2017-01-15 22:29:26 +0300

    external ip lib changed

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 const PORT = 8888
 
 var http = require('http');
-var externalip = require('externalip');
+var getIP = require('external-ip');
 
 var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    externalip(function (err, ip) {
+    getIP(function (err, ip) {
+        if (err) {
+            throw err;
+        }
         response.end(ip);
     });
 });
